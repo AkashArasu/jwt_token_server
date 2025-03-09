4e0760c60a4f6cb5b5131f88a19ebc5d58eed33b
AkashArasu <takash.arasu@gmail.com>
2025-03-08 22:55:07 -0800

    Second commit

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from getstream import Stream
+import os
+from dotenv import load_dotenv
 import json
 
+load_dotenv()
 app = Flask(__name__)
 
+
 @app.route('/')
+def home():
+    return os.getenv("API_KEY_NAME") + " " + os.getenv("API_SECRET_NAME")
+
+@app.route('/get-token', methods=['GET'])
 def generate_token(request):
-    client = Stream(api_key="y8h5754fwgma", api_secret="zg87h9juqaqk8gxn6mrk3a5hkgkyxvh7rkkjke9wcufwxc9ukub6vp75qnymkrwp", timeout=3.0)
+    client = Stream(api_key=os.getenv("API_KEY_NAME"), api_secret=os.getenv("API_SECRET_NAME"), timeout=3.0)
 
     # user_id = request.args.get('user_id')
     
