92763b4ab1eb513c3a107129fdc640afe95f86df
AkashArasu <takash.arasu@gmail.com>
2025-03-08 23:40:32 -0800

    formatted getter url

--- app.py
+++ app.py
@@ -10,16 +10,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return os.getenv("API_KEY_NAME") + " " + os.getenv("API_SECRET_NAME")
+    return "Hello World"
 
-@app.route('/get-token', methods=['GET'])
-def generate_token(request):
+@app.route('/get-token/<user_id>', methods=['GET'])
+def generate_token(user_id):
+
     client = Stream(api_key=os.getenv("API_KEY_NAME"), api_secret=os.getenv("API_SECRET_NAME"), timeout=3.0)
-
-    # user_id = request.args.get('user_id')
-    
-    user_id = request.user.id
-
     token = client.create_token(user_id)
     return json.dumps({"token": token})
 
